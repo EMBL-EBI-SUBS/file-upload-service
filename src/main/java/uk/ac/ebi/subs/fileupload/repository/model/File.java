2d40b8b034f93b57544184901abf0f0d1060ef48
Karoly Erdos <karoly@ebi.ac.uk>
2018-01-03 16:08:54 +0000

    Add model and repository for storing information about uploaded files

---
+++ src/main/java/uk/ac/ebi/subs/fileupload/repository/model/File.java
@@ -0,0 +1,36 @@
+package uk.ac.ebi.subs.fileupload.repository.model;
+
+import lombok.Data;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import uk.ac.ebi.subs.fileupload.util.FileStatus;
+
+/**
+ * This MongoDB Document describes information of a file to be uploaded to the various biological archives.
+ */
+@Document
+@Data
+public class File {
+
+    private long id;
+
+    @Indexed
+    private String tusId;
+
+    @Indexed
+    private String filename;
+
+    private String filePath;
+
+    @Indexed
+    private String submissionId;
+
+    private long totalSize;
+
+    private long uploadedSize;
+
+    @Indexed
+    private String user;
+
+    private FileStatus status;
+}
