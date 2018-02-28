43c280e8e3781f18faf39e7526f7ed9d05c0f813
Karoly Erdos <karoly@ebi.ac.uk>
2018-02-28 13:31:25 +0000

    Add missing toString method

--- src/main/java/uk/ac/ebi/subs/fileupload/model/ChecksumGenerationMessage.java
+++ src/main/java/uk/ac/ebi/subs/fileupload/model/ChecksumGenerationMessage.java
@@ -1,9 +1,12 @@
 package uk.ac.ebi.subs.fileupload.model;
 
+import lombok.ToString;
+
 /**
  * This class represents a RabbitMQ JSON message.
  * It only contains 1 key-value pair that contains the file's generated ID by TUS.
  */
+@ToString
 public class ChecksumGenerationMessage {
 
     private String generatedTusId;
