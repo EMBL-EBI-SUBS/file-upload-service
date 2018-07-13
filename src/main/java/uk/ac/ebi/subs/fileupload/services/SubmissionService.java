be31c40da26920ff15f87324829afe8ad208fdd2
Karoly Erdos <karoly@ebi.ac.uk>
2018-07-13 11:07:30 +0100

    Removed the unneeded interfaces from the various services

--- src/main/java/uk/ac/ebi/subs/fileupload/services/SubmissionService.java
+++ src/main/java/uk/ac/ebi/subs/fileupload/services/SubmissionService.java
@@ -1,14 +1,71 @@
 package uk.ac.ebi.subs.fileupload.services;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+import uk.ac.ebi.subs.data.status.SubmissionStatusEnum;
 import uk.ac.ebi.subs.fileupload.errors.SubmissionNotFoundException;
+import uk.ac.ebi.subs.repository.model.SubmissionStatus;
 
 /**
  * This class is responsible for provide information about {@code Submission}s.
  * Created by karoly on 18/12/2017.
  */
-public interface SubmissionService {
+@Service
+public class SubmissionService {
 
-    String getSubmissionStatus(String submissionId, String jwtToken) throws SubmissionNotFoundException;
+    private RestTemplate restTemplate;
 
-    boolean isModifiable(String submissionId, String jwtToken) throws SubmissionNotFoundException;
+    @Value("${file-upload.subs-api.host}")
+    private String serviceHost;
+    @Value("${file-upload.subs-api.submissionStatusURI}")
+    private String submissionStatusURI;
+    @Value("${file-upload.subs-api.submissionURI}")
+    private String submissionURI;
+
+    public SubmissionService(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    public String getSubmissionStatus(String submissionUuid, String jwtToken) throws SubmissionNotFoundException {
+        ResponseEntity<SubmissionStatus> submissionStatusResponse;
+        try {
+            submissionStatusResponse = restTemplate.exchange(
+                    String.format(submissionStatusURI, serviceHost, submissionUuid),
+                    HttpMethod.GET,
+                    createRequestEntity(jwtToken),
+                    SubmissionStatus.class);
+        } catch (HttpClientErrorException httpClientException) {
+            if (httpClientException.getStatusCode().equals(HttpStatus.NOT_FOUND)) {
+                throw new SubmissionNotFoundException(submissionUuid);
+            } else {
+                throw httpClientException;
+            }
+        }
+
+        return submissionStatusResponse.getBody().getStatus();
+    }
+
+    public boolean isModifiable(String submissionUuid, String jwtToken) throws SubmissionNotFoundException {
+
+        return getSubmissionStatus(submissionUuid, jwtToken).equals(SubmissionStatusEnum.Draft.name());
+    }
+
+    private HttpEntity<?> createRequestEntity(String jwtToken) {
+        MultiValueMap<String,String> headers = new LinkedMultiValueMap<>();
+        headers.add("Authorization", "Bearer " + jwtToken);
+        headers.add("Content-Type", MediaTypes.HAL_JSON_VALUE);
+        headers.add("Accept", MediaTypes.HAL_JSON_VALUE);
+
+        return new HttpEntity<>(headers);
+    }
 }
--- src/main/java/uk/ac/ebi/subs/fileupload/services/ValidationService.java
+++ src/main/java/uk/ac/ebi/subs/fileupload/services/ValidationService.java
@@ -1,25 +1,110 @@
 package uk.ac.ebi.subs.fileupload.services;
 
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitMessagingTemplate;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import uk.ac.ebi.subs.fileupload.errors.ErrorMessages;
+import uk.ac.ebi.subs.fileupload.errors.ErrorResponse;
 import uk.ac.ebi.subs.fileupload.model.TUSFileInfo;
+import uk.ac.ebi.subs.messaging.Exchanges;
 import uk.ac.ebi.subs.repository.model.fileupload.File;
+import uk.ac.ebi.subs.repository.repos.fileupload.FileRepository;
+import uk.ac.ebi.subs.validator.data.FileUploadValidationEnvelopeToCoordinator;
+import uk.ac.ebi.subs.validator.data.ValidationResult;
+import uk.ac.ebi.subs.validator.repository.ValidationResultRepository;
+
+import java.util.UUID;
 
 /**
  * This class is responsible for validating the requests sent by the tusd server.
  */
-public interface ValidationService {
+@Service
+@RequiredArgsConstructor
+public class ValidationService {
+
+    @NonNull
+    private SubmissionService submissionService;
+    @NonNull
+    private RabbitMessagingTemplate rabbitMessagingTemplate;
+    @NonNull
+    private ValidationResultRepository validationResultRepository;
+    @NonNull
+    private FileRepository fileRepository;
+
+    private static final String FILE_REF_VALIDATION_ROUTING_KEY = "usi.file.created";
 
     /**
      * Check the validity of the file upload request.
      * First validate if the JWT token is valid, then check if the given submission exists and modifiable.
      *
-     * @param jwt the JWT security token from the original TUS client's file upload request
+     * @param jwtToken the JWT security token from the original TUS client's file upload request
      * @param submissionUuid the UUID of the submission the file would belong to
      * @return response entity with OK HTTP status if everything is fine, otherwise other HTTP error status
      */
-    ResponseEntity<Object> validateFileUploadRequest(String jwt, String submissionUuid);
+    public ResponseEntity<Object> validateFileUploadRequest(String jwtToken, String submissionUuid) {
 
-    ResponseEntity<Object> validateMetadata(TUSFileInfo.MetaData fileMetadata);
+        boolean isSubmissionModifiable = submissionService.isModifiable(submissionUuid, jwtToken);
 
-    void validateFileReference(String tusId);
+        if (!isSubmissionModifiable) {
+            return ErrorResponse.assemble(HttpStatus.UNPROCESSABLE_ENTITY,
+                    String.format(ErrorMessages.SUBMISSION_NOT_MODIFIABLE, submissionUuid));
+        }
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    public ResponseEntity<Object> validateMetadata(TUSFileInfo.MetaData fileMetadata) {
+        String jwtToken = fileMetadata.getJwtToken();
+        String submissionId = fileMetadata.getSubmissionID();
+        String filename = fileMetadata.getFilename();
+
+        ResponseEntity<Object> response = new ResponseEntity<>(HttpStatus.OK);
+
+        if (StringUtils.isEmpty(jwtToken)) {
+            return ErrorResponse.assemble(HttpStatus.UNPROCESSABLE_ENTITY, ErrorMessages.JWT_TOKEN_MANDATORY);
+        }
+
+        if (StringUtils.isEmpty(submissionId)) {
+            return ErrorResponse.assemble(HttpStatus.UNPROCESSABLE_ENTITY, ErrorMessages.SUBMISSION_ID_MANDATORY);
+        }
+
+        if (StringUtils.isEmpty(filename)) {
+            return ErrorResponse.assemble(HttpStatus.UNPROCESSABLE_ENTITY, ErrorMessages.FILENAME_MANDATORY);
+        }
+
+        return response;
+    }
+
+    public void validateFileReference(String tusId) {
+        File persistedFile = fileRepository.findByGeneratedTusId(tusId);
+        createValidationResult(persistedFile);
+        sendFileReferenceValidationEvent(persistedFile);
+    }
+
+    private void createValidationResult(File file) {
+        ValidationResult validationResult = new ValidationResult();
+        validationResult.setEntityUuid(file.getId());
+        validationResult.setUuid(UUID.randomUUID().toString());
+
+        validationResult.setSubmissionId(file.getSubmissionId());
+        validationResultRepository.save(validationResult);
+
+        String tusId = file.getGeneratedTusId();
+        File persistedFile = fileRepository.findByGeneratedTusId(tusId);
+
+        persistedFile.setValidationResult(validationResult);
+
+        fileRepository.save(persistedFile);
+    }
+
+    private void sendFileReferenceValidationEvent(uk.ac.ebi.subs.data.fileupload.File file) {
+        FileUploadValidationEnvelopeToCoordinator validationEnvelope =
+                new FileUploadValidationEnvelopeToCoordinator(file.getSubmissionId(), file);
+
+        rabbitMessagingTemplate.convertAndSend(Exchanges.SUBMISSIONS, FILE_REF_VALIDATION_ROUTING_KEY, validationEnvelope);
+    }
 }
