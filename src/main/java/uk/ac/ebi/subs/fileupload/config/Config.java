9b4a32cd0fb565cdfb66cb286f3bc3ee8050bf72
M. Haseeb <mhaseeb@ebi.ac.uk>
2020-02-26 15:31:13 +0000

    SUBS-1837
    - some improvements and changes for running the service locally.

--- src/main/java/uk/ac/ebi/subs/fileupload/config/Config.java
+++ src/main/java/uk/ac/ebi/subs/fileupload/config/Config.java
@@ -3,13 +3,24 @@ package uk.ac.ebi.subs.fileupload.config;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.web.client.DefaultResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
 
 @Configuration("FileUploadServiceConfiguration")
 public class Config {
 
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplateBuilder().setConnectTimeout(30_000).build();
+    public RestTemplate globusRestTemplate() {
+        return new RestTemplateBuilder()
+                .errorHandler(new DefaultResponseErrorHandler(){
+                    @Override
+                    public void handleError(ClientHttpResponse response) throws IOException {
+                    }
+                })
+                .setReadTimeout(30_000)
+                .setConnectTimeout(30_000).build();
     }
 }
--- src/test/java/uk/ac/ebi/subs/fileupload/services/globus/GlobusApiClientTest.java
+++ src/test/java/uk/ac/ebi/subs/fileupload/services/globus/GlobusApiClientTest.java
@@ -5,13 +5,16 @@ import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import uk.ac.ebi.subs.fileupload.config.Config;
 
 @Ignore
 @RunWith(SpringRunner.class)
-@ContextConfiguration(classes = {Config.class, GlobusApiClient.class})
+@ContextConfiguration(classes = {Config.class, GlobusApiClient.class}, initializers = ConfigFileApplicationContextInitializer.class)
+@TestPropertySource(locations="classpath:application.yml")
 public class GlobusApiClientTest {
 
     @Autowired
