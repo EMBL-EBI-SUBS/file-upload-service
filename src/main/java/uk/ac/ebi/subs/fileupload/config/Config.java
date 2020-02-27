7de072cff2fac56f1492c067debd33c67e333024
M. Haseeb <mhaseeb@ebi.ac.uk>
2020-02-27 17:09:03 +0000

    SUBS-1837
    - added proxy configuration for rest template.

--- src/main/java/uk/ac/ebi/subs/fileupload/config/Config.java
+++ src/main/java/uk/ac/ebi/subs/fileupload/config/Config.java
@@ -1,9 +1,13 @@
 package uk.ac.ebi.subs.fileupload.config;
 
+import org.apache.http.HttpHost;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.DefaultResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,9 +16,26 @@ import java.io.IOException;
 @Configuration("FileUploadServiceConfiguration")
 public class Config {
 
+    @Value("${file-upload.globus.proxy.host:#{null}}")
+    private String proxyHost;
+
+    @Value("${file-upload.globus.proxy.port:#{null}}")
+    private Integer proxyPort;
+
     @Bean
     public RestTemplate globusRestTemplate() {
-        return new RestTemplateBuilder()
+        RestTemplateBuilder restTemplateBuilder = new RestTemplateBuilder();
+
+        if (proxyHost != null && proxyPort != null) {
+            HttpHost proxy = new HttpHost(proxyHost, proxyPort);
+
+            HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+            requestFactory.setHttpClient(HttpClientBuilder.create().setProxy(proxy).build());
+
+            restTemplateBuilder.requestFactory(requestFactory);
+        }
+
+        return restTemplateBuilder
                 .errorHandler(new DefaultResponseErrorHandler(){
                     @Override
                     public void handleError(ClientHttpResponse response) throws IOException {
