1e0e9936fea32bd897ad8225fbb587e499344e8c
M. Haseeb <mhaseeb@ebi.ac.uk>
2020-06-05 20:56:35 +0100

    SUBS-1837
    - review followup.

--- src/main/java/uk/ac/ebi/subs/fileupload/util/Utils.java
+++ src/main/java/uk/ac/ebi/subs/fileupload/util/Utils.java
@@ -3,15 +3,10 @@ package uk.ac.ebi.subs.fileupload.util;
 public class Utils {
 
     public static String generateFolderName(String submissionUUID) {
-        String separator = System.getProperty("file.separator");
 
-        StringBuilder folderName = new StringBuilder();
-        folderName.append(submissionUUID.substring(0, 1));
-        folderName.append(separator);
-        folderName.append(submissionUUID.substring(1, 2));
-        folderName.append(separator);
-        folderName.append(submissionUUID);
-
-        return folderName.toString();
+        return String.join(System.getProperty("file.separator"),
+                submissionUUID.substring(0, 1),
+                submissionUUID.substring(1, 2),
+                submissionUUID);
     }
 }
